b0b12922b327ff1fbd9840bdb625bb4945a662a7
LOKESH M <lokeshmalairaj@gmail.com>
2023-12-11 10:30:21 +0530

    c

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
     <Box m="20px">
       {/* HEADER */}
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Header title="Predictive Maintaince" subtitle="Know your vehicle health" />
 
         <Box>
           <Button
