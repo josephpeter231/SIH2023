43009fc39fcbb537c0b7e7e15110ea5322a4dc70
LOKESH M <lokeshmalairaj@gmail.com>
2023-12-11 14:21:04 +0530

    commited by loky

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -6,7 +6,6 @@ import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import './Topbar.css';
 
 const Topbar = () => {
   const theme = useTheme();
