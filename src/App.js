ad958fc7d70cfd87067ff80493019383af5f0298
josephpeter231 <josephpeterjcit@gmail.com>
2023-12-13 09:11:00 +0530

    removed axios

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from './Home';
 import Intro from './scenes/Intro/Driver';
 import Secondpage from './scenes/Intro/Luxury';
-import axios from 'axios';
 
-axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8000';
+
+
 
 const App = () => {
     return (  
